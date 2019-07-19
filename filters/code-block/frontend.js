1776eb4d9c836bc172ced80a4f6d895f9791a149
Julian Weiland <derweili@users.noreply.github.com>
2019-07-19 09:04:42 +0200

    Debug messages removed

--- filters/code-block/frontend.js
+++ filters/code-block/frontend.js
@@ -1,39 +1,20 @@
-/**
- * TODO: Frontend Code for Syntax Highlighting
- */
-console.log('frontend syntax highlighting');
-
 const { render } = wp.element;
 
 import Highlight from 'react-highlight.js'
 
 
-const codeElements = document.querySelectorAll(
-".derweili-gutenberg-code-highlighter"
-);
-codeElements.forEach((codeElement, index) => {
-    console.log('codeElement', codeElement);
-    const language = codeElement.dataset.language;
-    console.log(language);
-    const code = codeElement.getElementsByTagName('pre')[0].childNodes[0].innerText;
-    console.log(code);
+const codeElements = document.querySelectorAll( ".derweili-gutenberg-code-highlighter" );
 
-    // const islightboxenabled = codeElement.dataset.islightboxenabled;
-    // const images = codeElement.querySelectorAll("img");
-    // const photos = [];
-    // images.forEach(image => {
-    //     photos.push({
-    //     src: image.src,
-    //     width: image.width,
-    //     height: image.height,
-    //     alt: image.alt,
-    //     caption: image.title
-    //     });
-    // });
+codeElements.forEach( ( codeElement, index ) => {
+    
+    const language = codeElement.dataset.language;
+    const code = codeElement.getElementsByTagName( 'pre' )[0].childNodes[0].innerText;
+
     render(
         <Highlight language={language}>
             {code}
         </Highlight>,
         codeElement
     );
+
 });
